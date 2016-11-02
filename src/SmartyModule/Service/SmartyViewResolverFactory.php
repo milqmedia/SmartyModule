0dc866ff2ca8c45cc3634ecc288e93c7a2be3c86
Yo-han <johankuijt@gmail.com>
2016-11-02 16:53:46 +0100

    Update SmartyViewResolverFactory.php

--- src/SmartyModule/Service/SmartyViewResolverFactory.php
+++ src/SmartyModule/Service/SmartyViewResolverFactory.php
@@ -16,8 +16,8 @@ class SmartyViewResolverFactory implements FactoryInterface
     public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = NULL)
     {
         $resolver = new ViewResolver\AggregateResolver();
-        $resolver->attach($serviceLocator->get('SmartyViewTemplateMapResolver'));
-        $resolver->attach($serviceLocator->get('SmartyViewTemplatePathStack'));
+        $resolver->attach($container->get('SmartyViewTemplateMapResolver'));
+        $resolver->attach($container->get('SmartyViewTemplatePathStack'));
         return $resolver;
     }
     
